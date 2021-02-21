067eae862434c6945140787ae01c00ee66069c33
Buchikin <jirafanegra124@ezweb.ne.jp>
2021-02-22 02:25:35 +0900

    新規登録でのコントローラーの設定

--- app/controllers/sellers/registrations_controller.rb
+++ app/controllers/sellers/registrations_controller.rb
@@ -1,8 +1,16 @@
 # frozen_string_literal: true
 
 class Sellers::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
+
+  def new
+    @seller = Seller.new
+    @store = @store.build_profile
+  end
+
+
+
 
   # GET /resource/sign_up
   # def new
@@ -41,9 +49,9 @@ class Sellers::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:store_id, :store_name, store_attributes: [:category_id, :zip_code, :address, :phone_number, :url, :information, :responsible, :responsible_kana, :responsible_phone ] ])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
