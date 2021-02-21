eeebe48319278c31425ab7d38616affefb2b6896
Buchikin <jirafanegra124@ezweb.ne.jp>
2021-02-22 05:51:39 +0900

    モデルの修正

--- app/models/seller.rb
+++ app/models/seller.rb
@@ -2,7 +2,7 @@ class Seller < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :registerable
-  devise :database_authenticatable, :authentication_keys => [:store_id]              
+  devise :database_authenticatable, :authentication_keys => [:store_id]
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze  #半角英数どちらも含む
   validates_format_of :password, with: PASSWORD_REGEX, message: "must be included both letter(Half-width) and number", on: :create
@@ -10,7 +10,17 @@ class Seller < ApplicationRecord
 
   validates :store_id, presence: true
   validates :store_name, presence: true
+  validates :store_id, uniqueness: true
 
   has_one :store
   accepts_nested_attributes_for :store, update_only: true
+
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
+
 end
