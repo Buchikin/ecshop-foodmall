e44958438641ac71d02e52793b0a9618f45938c7
Buchikin <jirafanegra124@ezweb.ne.jp>
2021-02-21 18:46:47 +0900

    バリデーションを設定

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,24 @@
 class Profile < ApplicationRecord
 
+  with_options presence: true do
+
+    with_options format: { with: /\A[ぁ-んァ-ヶー一-龥]+\z/, message: "must be Full-width characters"} do
+      validates :last_name
+      validates :first_name
+    end
+
+    with_options format: { with: /\A[ァ-ヶー]+\z/, message: "must be Full-width katkana characters"} do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
+
+    validates :birth_day
+    validates :phone_number
+    validates :zip_code
+    validates :address
+
+  end
+
   belongs_to :user
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,13 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+          :recoverable, :rememberable, :validatable
+
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze  #半角英数どちらも含む
+  validates_format_of :password, with: PASSWORD_REGEX, message: "must be included both letter(Half-width) and number"
+
+  validates :nickname, presence: true
 
   has_one :profile
   accepts_nested_attributes_for :profile
