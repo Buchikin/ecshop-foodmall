7fd2c5df6cc746699210cf5dfaeb1260716863fb
Buchikin <jirafanegra124@ezweb.ne.jp>
2021-02-22 02:18:02 +0900

    バリデーションの設定

--- app/models/seller.rb
+++ app/models/seller.rb
@@ -3,4 +3,14 @@ class Seller < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze  #半角英数どちらも含む
+  validates_format_of :password, with: PASSWORD_REGEX, message: "must be included both letter(Half-width) and number", on: :create
+  validates_format_of :store_id, with: PASSWORD_REGEX, message: "must be included both letter(Half-width) and number"
+
+  validates :store_id, presence: true
+  validates :store_name, presence: true
+
+  has_one :store
+  accepts_nested_attributes_for :store, update_only: true
 end
