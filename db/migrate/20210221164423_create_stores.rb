da518d25a158f80deebc587845c6869f436d839f
Buchikin <jirafanegra124@ezweb.ne.jp>
2021-02-22 01:59:05 +0900

    storeモデルとテーブルの作成

---
+++ db/migrate/20210221164423_create_stores.rb
@@ -0,0 +1,17 @@
+class CreateStores < ActiveRecord::Migration[6.0]
+  def change
+    create_table :stores do |t|
+      t.integer :category_id, null: false
+      t.string :zip_code, null: false
+      t.string :address, null: false
+      t.string :phone_number, null: false
+      t.string :url
+      t.text :information, null: false
+      t.string :responsible, null: false
+      t.string :responsible_kana, null: false
+      t.string :responsible_phone, null: false
+      t.references :seller, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_20_092034) do
+ActiveRecord::Schema.define(version: 2021_02_21_164423) do
 
   create_table "profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "last_name", null: false
@@ -27,6 +27,33 @@ ActiveRecord::Schema.define(version: 2021_02_20_092034) do
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
+  create_table "sellers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "store_id", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "store_name", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "stores", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.integer "category_id", null: false
+    t.string "zip_code", null: false
+    t.string "address", null: false
+    t.string "phone_number", null: false
+    t.string "url"
+    t.text "information", null: false
+    t.string "responsible", null: false
+    t.string "responsible_kana", null: false
+    t.string "responsible_phone", null: false
+    t.bigint "seller_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["seller_id"], name: "index_stores_on_seller_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -39,4 +66,5 @@ ActiveRecord::Schema.define(version: 2021_02_20_092034) do
   end
 
   add_foreign_key "profiles", "users"
+  add_foreign_key "stores", "sellers"
 end
