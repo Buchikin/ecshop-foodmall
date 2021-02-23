56bfc808535712c53c5e43aab17696a253107ed1
Buchikin <jirafanegra124@ezweb.ne.jp>
2021-02-23 18:17:38 +0900

    バリデーションの追加

--- app/models/new_item.rb
+++ app/models/new_item.rb
@@ -21,6 +21,14 @@ class NewItem
     end
   end
 
+  with_options length: { maximum: 5, message: "must be 5 digits or less" }  do
+    validates :tag1
+    validates :tag2
+    validates :tag3
+    validates :tag4
+    validates :tag5
+  end
+
   def save
     item = Item.create(name: name, price: price, information: information, tag1: tag1, tag2: tag2, tag3: tag3, tag4: tag4, tag5: tag5, allergies: allergies, prefecture_id: prefecture_id, ship_day_id: ship_day_id, ship_type_id: ship_type_id, seller_id: seller_id, :image)
     Stock.create(count: count, item_id: item.id)
