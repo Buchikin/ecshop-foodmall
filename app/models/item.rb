cb8859d168ab72d3cfe9204b6c7bf5acb1062640
Buchikin <jirafanegra124@ezweb.ne.jp>
2021-02-23 17:12:34 +0900

    アソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,12 @@
 class Item < ApplicationRecord
 
-  
+
+  belongs_to :seller
+  has_one :stock
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+  belongs_to :ship_day
+  belongs_to :ship_type
 end
