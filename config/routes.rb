ffec699cd486b5d1664573f68daa7135f45db79d
Buchikin <jirafanegra124@ezweb.ne.jp>
2021-02-26 16:19:22 +0900

    モデルとテーブルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
     resources :stocks, only: [:edit, :update]
   end
 
-  
+  resources :charge, only: [:edit, :update]
 
   devise_for :users, controllers: {
     sessions:      'users/sessions',
