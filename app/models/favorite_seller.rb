5c2abb0907fa5267be756556cc8218c2e05bae07
Buchikin <jirafanegra124@ezweb.ne.jp>
2021-03-02 21:29:25 +0900

    モデルとテーブルの作成

---
+++ app/models/favorite_seller.rb
@@ -0,0 +1,4 @@
+class FavoriteSeller < ApplicationRecord
+
+  
+end
---
+++ db/migrate/20210302122808_create_favorite_sellers.rb
@@ -0,0 +1,10 @@
+class CreateFavoriteSellers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :favorite_sellers do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :seller, null: false, foreign_key: true
+      t.boolean :checked
+      t.timestamps
+    end
+  end
+end
