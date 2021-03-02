b7ad3359e3367718d700ac37856621835cbe2e1b
Buchikin <jirafanegra124@ezweb.ne.jp>
2021-03-02 11:21:18 +0900

    ユーザー側の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,40 @@
 class MessagesController < ApplicationController
+
+  def index
+    @messages = Message.where(params[:room_id])
+    @message = Message.new
+    @room = Room.find(params[:room_id])
+    @items = Item.where(seller_id: @room.seller.id)
+    set_item_name
+
+  end
+
+  def create
+    @room = Room.find(params[:room_id])
+    @message = Message.new(message_params)
+    if @message.valid?
+      @message.save
+      redirect_to room_messages_path(@room.id)
+    else
+      @messages = Message.where(params[:room_id])
+      @items = Item.where(seller_id: @room.seller.id)
+      set_item_name
+      render :index
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:item_name, :message).merge(room_id: @room.id, whom: current_user.nickname)
+  end
+
+  def set_item_name
+    @item_name = []
+    @items.each do |item|
+      @item_name.push(item.name)
+    end
+    @item_name.push("その他")
+  end
+  
 end
--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,2 +1,17 @@
 class RoomsController < ApplicationController
+
+  def index
+    @rooms = current_user.rooms
+  end
+
+  def create
+    if Room.exists?(user_id: "#{current_user.id}", seller_id: params[:format])
+      @room = Room.find_by(user_id: "#{current_user.id}", seller_id: params[:format])
+      redirect_to room_messages_path(@room.id)
+    else
+      @room = Room.create(user_id: current_user.id, seller_id: params[:format])
+      redirect_to room_messages_path(@room.id)
+    end
+  end
+
 end
